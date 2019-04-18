00c7bc1140bcd287fb61d3dfb995fe401cc2617a
loading-torrent-9632 <angelrey@buffalo.edu>
2019-04-18 14:37:18 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,4 +4,5 @@ end
 def current_player(board)
   # call the `#turn_count` method; If the turn count is an .even? number,
   # the `#current_player` method should return `"X"`, otherwise, it .odd? should return `"O"`
+  # condition ? if true : if false is the same as if condition true else false
 end
