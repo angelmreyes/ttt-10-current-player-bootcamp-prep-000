306626050efa169f4e9cba05a68883486951a3a7
loading-torrent-9632 <angelrey@buffalo.edu>
2019-04-18 14:48:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count(board)
   counter = 0
-  board.each { |token| if token == 'X' || token == 'O' then counter += 1 }
+  board.each { |token| if token == 'X' || token == 'O' then counter += 1 end }
   counter
 end
 
